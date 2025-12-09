e5db7f15fac0a535cbdb604978c7eccd76323687
Carlos Castillo Sánchez <ccastillosanchez2003@gmail.com>
2025-12-09 19:15:45 +0100

    Comentarios tipo Doxygen en Dictionary.h y letters_bag.h

--- include/dictionary.h
+++ include/dictionary.h
@@ -10,71 +10,151 @@ using namespace std;
 /**
  * @brief TDA Dictionary
  * @details Almacena las palabras de un fichero de texto y permite iterar sobre ellas
- *
- */
+**/
 
 class Diccionario{
 private:
+    /**
+     * @brief Estructura lineal base para el diccionario. Consiste en un set de string, ya que se
+     * trata de palabras, y no tiene sentido repetirlas.
+    **/
     set<string> datos;
 
 public:
     /**
-    * @brief Construye un diccionario vacío.
+    * @brief Diccionario()
+    * @details Construye un diccionario vacío.
     **/
     Diccionario();
 
     /**
-    * @brief Devuelve el número de palabras en el diccionario
+     * @brief int size() const
+     * @details Devuelve el número de palabras en el diccionario
     **/
     int size() const ;
 
     /**
-    * @brief Obtiene todas las palabras en el diccionario de un longitud dada
+    * @brief vector<string> PalabrasLongitud(int longitud)
+    * @details Obtiene todas las palabras en el diccionario de un longitud dada
     * @param longitud la longitud de las palabras de salida
     * @return un vector con las palabras de longitud especifica en el parametro de entrada
     **/
-
     vector<string> PalabrasLongitud(int longitud);
+
     /**
-    * @brief Indica si una palabra está en el diccionario o no
+    * @brief bool Esta(const string & palabra) const.
+    * @details Indica si una palabra está en el diccionario o no.
     * @param palabra la palabra que se quiere buscar
     * @return true si la palabra está en el diccionario. False en caso contrario
     **/
     bool Esta(const string & palabra) const;
 
     /**
-    * @brief Lee de un flujo de entrada un diccionario
-    * @param is flujo de entrada
+    * @brief friend istream & operator>> (istream & is,Diccionario & D).
+    * @details Lee de un flujo de entrada un diccionario.
+    * @param is flujo de entrada.
     * @param D el objeto donde se realiza la lectura.
-    * @return el flujo de entrada
+    * @return el flujo de entrada.
     **/
     friend istream & operator>>(istream & is,Diccionario &D);
 
     /**
-    * @brief Escribe en un flujo de salida un diccionario
-    * @param os flujo de salida
-    * @param D el objeto diccionario que se escribe
-    * @return el flujo de salida
+    * @brief friend ostream & operator<< (ostream & os, const Diccionario & D).
+    * @details Escribe en un flujo de salida un diccionario.
+    * @param os flujo de salida.
+    * @param D el objeto diccionario que se escribe.
+    * @return el flujo de salida.
     **/
     friend ostream & operator<<(ostream & os, const Diccionario &D);
 
 
+    /**
+     * @brief Clase iteradora para el TDA Diccionario.
+     * @details Se basa en un set<string>::iterator.
+    **/
     class iterator{
 
     private:
+        /**
+         * @brief set<string>::iterator it
+         * @details iterador de la clase set<string>::iterator como base para el iterador del
+         * TDA Diccionario.
+        **/
         set<string>::iterator it;
 
     public:
+        /**
+         * @brief iterator().
+         * @details Constructor por defecto.
+         */
         iterator ();
+
+        /**
+         * @brief const string & operator* () const.
+         * @return  Devuelve el elemento del diccionario al que apunta el iterador.
+         * @note Método de consulta.
+         */
         const string & operator*() const;
+
+        /**
+         * @brief iterator & operator++ ().
+         * @details Calcula la posición siguiente del diccionario. Como es de imaginar, es la dirección
+         * de la siguiente palabra del diccionario.
+         * @return Puntero a la siguiente posición según la indicación anterior.
+         */
         iterator & operator ++();
+
+        /**
+         * @brief bool operator== (const iterator &i).
+         * @details Calcula si la palabra del diccionario a la que apunta el iterador implícito
+         * es igual a la que apunta el parámetro \par i.
+         * @param i
+         * @return true si ambas son iguales; false en caso contrario.
+         */
         bool operator ==(const iterator &i);
+
+        /**
+         * @brief bool operator !=(const iterator &i).
+         * @details Calcula si las palabras a las que apuntan el iterador implícito y el iterador
+         * del parámetro @par i son diferentes.
+         * @param i
+         * @return true si las palabras son diferentes; false si son iguales.
+         */
         bool operator !=(const iterator &i);
+
+        /**
+         * @brief friend class Diccionario
+         * @details La clase Diccionario se declara como amiga de la iteradora para
+         * poder acceder a los miembros privados del diccionario, que en este caso es
+         * todo el set de palabras.
+         */
         friend class Diccionario;
     };
 
+    /**
+     * @brief const iterator begin() const.
+     * @details Calcula la primera posición (dirección de la palabra
+     * correspondiente) del diccionario.
+     * @return Un puntero a la primera palabra del diccionario.
+     */
     const iterator begin() const;
+
+    /**
+     * @brief const iterator end() const.
+     * @details Calcula la última posición (dirección de la palabra
+     * correspondiente) del diccionario.
+     * @return Un puntero a la última palabra del diccionario.
+     */
     const iterator end() const;
 };
+
+/**
+ * @brief string mayusculas (string & palabra)
+ * @details Pasa a mayúsculas todos los caracteres de la palabra que se
+ * pasa por parámetro.
+ * @param palabra Palabra que se va a pasar a mayúsculas.
+ * @note Método que modifica.
+ * @return La palabra en mayúsculas.
+ */
 string mayusculas(string &palabra);
 #endif
--- include/letters_bag.h
+++ include/letters_bag.h
@@ -8,13 +8,17 @@ using namespace std;
 
 /**
  * @brief TDA LettersBag
- *
- * Este TDA almacena un conjunto de chars utilizado en el juego de letras.
- * La estructura de datos subyacente es una lista de chars.
+ * @details Este TDA almacena un conjunto de char utilizado en el juego de letras.
+ * La estructura de datos subyacente es una string.
  */
 
 class BolsaLetras {
 private:
+    /**
+     * @brief string bolsa_letras
+     * @details Conjunto de caracteres almacenados según la cantidad
+     * apariciones que determine un objeto ConjuntoLetras.
+     */
     string bolsa_letras;
 
 public:
@@ -25,39 +29,46 @@ public:
     BolsaLetras();
 
     /**
-     * @brief Calcula la cantidad total de elementos que hay en la bolsa
-     * @return Tamaño de la bolsa
+     * @brief int size() const
+     * @details Calcula la cantidad total de elementos que hay en la bolsa
+     * @return Tamaño de la bolsa.
+     * @note Método de consulta.
      */
     int size() const;
 
-    /**
-     * @brief Añade un elemento a la bolsa
+    /** @brief void add(Letra letra)
+     * @details Añade un elemento a la bolsa
      * @param letra Letra que se va a añadir a la bolsa
      */
     void add(Letra letra);
 
     /**
-     * @brief Extrae un elemento aleatorio de la bolsa y lo elimina de la
+     * @brief char get()
+     * @details Extrae un elemento aleatorio de la bolsa y lo elimina de la
      * bolsa.
      * @return Elemento aleatorio
      */
     char get();
 
     /**
-     * @brief Formatea la bolsa y la transforma en una cadena tipo std::string.
+     * @brief
+     * @details Formatea la bolsa y la transforma en una cadena tipo std::string.
      * @return Una cadena tipo std::string la bolsa formateada, de forma
      * que por cada elemento, se imprime una línea.
+     * @note Método de consulta.
      */
     string toString() const;
 
     /**
-     * @brief Cargar Bolsa
+     * @brief void cargarBolsa (const ConjuntoLetras & conj)
+     * @details Cargar Bolsa.
      * @param conj : conjunto de letras a partir del cual se crea la bolsa
      */
     void cargarBolsa (const ConjuntoLetras &conj) ;
 
     /**
-     * @brief Sobrecarga del operador <<. Posibilita la salida formateada
+     * @brief friend ostream & operator<<(ostream & os, const BolsaLetras & b)
+     * @details Sobrecarga del operador <<. Posibilita la salida formateada
      * de la bolsa por un flujo, que por defecto es la salida estándar.
      * @param os Flujo de salida. Por defecto, os = cout.
      * @param b Bolsa.
@@ -66,7 +77,8 @@ public:
     friend ostream& operator<<(ostream& os, const BolsaLetras& b);
 
     /**
-     * @brief Sobrecarga del operador >>. Permite la lectura de un objeto
+     * @brief friend istream & operator>>(istream & is, BolsaLetras & b)
+     * @details Sobrecarga del operador >>. Permite la lectura de un objeto
      * tipo BolsaLetras desde un flujo de entrada. Se usará para leer bolsas
      * que vienen formateadas según la salida del programa 'Conjunto_letras',
      * a saber: "[letra] [cantidad] [puntuación]" por cada línea/letra.
